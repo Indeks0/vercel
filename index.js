cc21f1829620c41542e346648fc0685dbaa4a4f6
Ivan Paradžik <ivanparadzik4@gmail.com>
2023-04-22 12:49:30 +0200

    add reading endpoint

--- index.js
+++ index.js
@@ -11,16 +11,35 @@ app.post("/reading", async (req, res) => {
     try {
         res.setHeader("Content-Type", "application/json");
         res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-        
+
         const { temperature, humidity, pressure, light, particles } = req.body;
         let data = [];
         data.push(temperature, humidity, pressure, light, particles);
-        
+
         const newReading = await pool.query(
             `INSERT INTO "reading" ("temperature", "humidity", "pressure", "light", "particles") VALUES($1, $2, $3, $4, $5)`,
             data
         );
-        res.json(newReading.rows);
+        return res.json(newReading.rows);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.get("/latest-reading", async (req, res) => {
+    try {
+        res.setHeader("Content-Type", "application/json");
+        res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+
+        const { temperature, humidity, pressure, light, particles } = req.body;
+        let data = [];
+        data.push(temperature, humidity, pressure, light, particles);
+
+        const newReading = await pool.query(
+            `SELECT id, temperature, humidity, pressure, light, particles, datecreated
+            FROM public.reading order by datecreated desc limit 1`
+        );
+        return res.json(newReading.rows);
     } catch (error) {
         console.log(error);
     }
